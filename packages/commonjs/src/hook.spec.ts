411cb7c7fbab1b7fb06913b8917bc578e6ff54f4
swimauger <swimauger@gmail.com>
2025-01-26 23:35:38 -0600

    Create Module Hooks Library v1.0.0

---
+++ packages/commonjs/src/hook.spec.ts
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2025 Mark Auger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import assert from 'node:assert';
+import fs from 'node:fs';
+import { describe, it, mock } from 'node:test';
+
+import { resolve } from './hook.js';
+import extensions from './json/extensions.json';
+
+describe(resolve.name, () => {
+  function assertDefaultModuleResolved(
+    mockSpecifier: string,
+    mockNextResolve: ReturnType<typeof mock['fn']>
+  ) {
+    assert.ok(mockNextResolve.mock.callCount(), 'module was never resolved');
+    const [ mockNextResolveCall ] = mockNextResolve.mock.calls;
+    const [ mockNextResolveCallSpecifier ] = mockNextResolveCall.arguments;
+    assert.strictEqual(
+      mockNextResolveCallSpecifier,
+      mockSpecifier,
+      'module was resolved with a different specifier than the default'
+    );
+  }
+
+  it('should not resolve a module with a file extension specified', () => {
+    const mockSpecifier = 'file:///path/to/module.js';
+    const mockContext = {
+      conditions: new Set([ 'require' ]),
+      importAttributes: null,
+      parentURL: null
+    };
+    const mockNextResolve = mock.fn();
+    resolve(mockSpecifier, mockContext, mockNextResolve);
+    assertDefaultModuleResolved(mockSpecifier, mockNextResolve);
+  });
+
+  it('should not resolve a module without a import', () => {
+    const mockSpecifier = 'file:///path/to/module';
+    const mockContext = {
+      conditions: new Set([]),
+      importAttributes: null,
+      parentURL: null
+    };
+    const mockNextResolve = mock.fn();
+    resolve(mockSpecifier, mockContext, mockNextResolve);
+    assertDefaultModuleResolved(mockSpecifier, mockNextResolve);
+  });
+
+  for (const extension of extensions) {
+    it(`should resolve a ${extension} module without a file extension`, () => {
+      const mockSpecifier = 'file:///path/to/module';
+      const mockContext = {
+        conditions: new Set([ 'require' ]),
+        importAttributes: null,
+        parentURL: null
+      };
+      const mockNextResolve = mock.fn();
+      mock.method(fs, 'existsSync', path =>
+        path === mockSpecifier + extension
+      );
+      resolve(mockSpecifier, mockContext, mockNextResolve);
+      assert.ok(mockNextResolve.mock.callCount(), 'module was never resolved');
+      const [ mockNextResolveCall ] = mockNextResolve.mock.calls;
+      const [ mockNextResolveCallSpecifier ] = mockNextResolveCall.arguments;
+      assert.strictEqual(
+        mockNextResolveCallSpecifier,
+        mockSpecifier + extension,
+        'module was not resolved with the correct extension'
+      );
+    });
+  }
+});
